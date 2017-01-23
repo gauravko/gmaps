d8c0ea8ea6735bc49e54508a2793829ecea7520e
Gaurav <gaurav.gprs@gmail.com>
2017-01-23 18:41:00 +0530

    working version of overlaying images

--- generateStaticMapImage.py
+++ generateStaticMapImage.py
@@ -1,15 +1,17 @@
-import requests
-from getZoomFromBounds import getBoundsZoomLevel, getCentre, calcBounds
+import requests, json, urllib, cStringIO
+from StringIO import StringIO
+from mapEngine import getBoundsZoomLevel, getCentre, calcBounds, getLatLngtoPixel
 from mercatorProjection import fromPointToLatLong, fromLatLongToPoint
 from PIL import Image, ImageDraw
 
+FLICKR_RANDOM_IMAGE_URL = 'http://lorempixel.com//80/60'
 _workingDirectory = 'temp/'
 
 def generateMap(markers, mapDim):
 
     zoom = getBoundsZoomLevel(markers, mapDim)
 
-    marker_args = '&markers='
+    marker_args = '&visible='
     firstMarker = True
     for marker in markers:
         if not firstMarker:
@@ -21,7 +23,7 @@ def generateMap(markers, mapDim):
         'http://maps.googleapis.com/maps/api/staticmap',
         '?style=feature:all|element:all|saturation:-80|gamma:0.4&sensor=false&v=3&visual_refresh=true',
         '&size={}x{}&zoom={}'.format(
-            mapDim['height'], mapDim['width'], zoom),
+            mapDim['width'], mapDim['height'], zoom),
         marker_args,])
 
     print(staticMapUrl)
@@ -31,25 +33,29 @@ def generateMap(markers, mapDim):
     file.write(r.content)
     file.close()
 
+    return zoom
+
+
+def generateCompositeImage(markers, mapDim, zoom):
     centreLatLng = getCentre(markers)
-
-    northEast, southWest = calcBounds( centreLatLng, zoom, mapDim['height'], mapDim['width'])
-
-    return northEast, southWest
-
-def generateCompositeImage(markers, northEast, southWest, mapDim):
-    northEastPoint = fromLatLongToPoint(northEast)
-    southWestPoint = fromLatLongToPoint(southWest)
 
     staticMapIm = Image.open(_workingDirectory + 'test.png')
     newImage = staticMapIm.copy()
-    thumbnail = Image.open(_workingDirectory + 'thumbnail.png')
 
+    #Using Flick random image generator
+    r = requests.get(FLICKR_RANDOM_IMAGE_URL)
+    thumbnail = Image.open(StringIO(r.content))
+    thumbnail.save(_workingDirectory + 'tempflick.png')
+
+    centrePx = getLatLngtoPixel(centreLatLng, zoom)
+
+    NorthWestPx = { 'x': centrePx['x'] - mapDim['width']/2, 'y': centrePx['y'] - mapDim['height']/2}
 
     for marker in markers:
-        markerPoint = fromLatLongToPoint(marker)
-        x_coordinate = int( mapDim['height'] + ((markerPoint['x'] - northEastPoint['x'])*mapDim['height'])/(northEastPoint['x'] - southWestPoint['x']))
-        y_coordinate = int(-((markerPoint['y'] - northEastPoint['y'])*mapDim['width'])/(northEastPoint['y'] - southWestPoint['y']))
+        markerPx = getLatLngtoPixel(marker, zoom)
+
+        x_coordinate = int((markerPx['x'] - NorthWestPx['x']))
+        y_coordinate = int(markerPx['y'] - NorthWestPx['y'])
 
         print(x_coordinate, y_coordinate)
         newImage.paste(thumbnail, (x_coordinate, y_coordinate))
@@ -59,17 +65,18 @@ def generateCompositeImage(markers, northEast, southWest, mapDim):
 def main():
 
     markers = [
-        {'lat': 41.007880, 'lng': 28.982449},
-        {'lat': 41.006959, 'lng': 29.011974},
-        {'lat': 41.037330, 'lng': 28.984423}
+        {'lat': 51.527085, 'lng': -0.132952},
+        {'lat': 51.512665, 'lng': -0.070810},
+        {'lat': 51.501981, 'lng': -0.103083},
+        {'lat': 51.505507, 'lng': -0.189772}
     ]
 
-    mapDim = {'height': 640, 'width': 480}
 
-    northEast, southWest = generateMap(markers, mapDim)
+    mapDim = {'height': 480, 'width': 640}
 
-    generateCompositeImage(markers, northEast, southWest, mapDim)
+    zoom = generateMap(markers, mapDim)
 
+    generateCompositeImage(markers, mapDim, zoom)
 
 
 if __name__ == '__main__':
@@ -82,5 +89,22 @@ Istanbul co-ordinates
         {'lat': 41.007880, 'lng': 28.982449},
         {'lat': 41.006959, 'lng': 29.011974},
         {'lat': 41.037330, 'lng': 28.984423}
+    ]
+
+London
+
+    markers = [
+        {'lat': 51.527085, 'lng': -0.132952},
+        {'lat': 51.512665, 'lng': -0.070810},
+        {'lat': 51.501981, 'lng': -0.103083},
+        {'lat': 51.505507, 'lng': -0.189772}
+    ]
+
+London, New Delhi
+
+    markers = [
+        {'lat': 51.527085, 'lng': -0.132952},
+        {'lat': 28.613939, 'lng': 77.209021}
+
     ]
 '''
---
+++ mapEngine.py
@@ -0,0 +1,116 @@
+import math
+import json
+import requests
+from mercatorProjection import fromPointToLatLong, fromLatLongToPoint
+
+EARTH_RADIUS = 6378137
+MIN_LAT = -85.05112878
+MAX_LAT = 85.05112878
+MIN_LNG = -180
+MAX_LNG = 180
+
+def latRad(latitude):
+    sin = math.sin( latitude * math.pi / 180 )
+    radx2 = math.log((1+sin)/(1-sin))/2
+    return max(min(radx2, math.pi), -math.pi)/2
+
+def calcZoom(mapPx, worldPx, fraction):
+    return round(math.log(mapPx / worldPx / fraction) / math.log1p(1))
+
+def latRad(lat):
+    sin = math.sin( lat * math.pi/180)
+    radx2 = math.log( (1 + sin)/ (1 - sin))/2
+    return max( min(radx2, math.pi), -math.pi)/2
+
+def calcZoom(mapPx, worldPx, fraction):
+    print(mapPx, worldPx, fraction)
+    return math.floor(math.log( mapPx/worldPx/fraction)/ math.log1p(1))
+
+def getBoundaries(bounds):
+
+    northEast = { 'lat' : max(bounds, key=lambda x:x['lat'])['lat'], 'lng' : max(bounds, key=lambda x:x['lng'])['lng']}
+    southWest = {'lat': min(bounds, key=lambda x: x['lat'])['lat'], 'lng': min(bounds, key=lambda x: x['lng'])['lng']}
+
+    return northEast, southWest
+
+def calcMapSize(zoom):
+    return 256 << zoom
+
+def clip(n, minValue, maxValue):
+    return min(max(n, minValue), maxValue)
+
+def LatLongttoPixel(latLng, zoom):
+    lat = clip(latLng['lat'], MIN_LAT, MAX_LAT)
+    lng = clip(latLng['lng'], MIN_LNG, MAX_LNG)
+
+    x = (lng + 180)/360
+
+    siny = math.sin(lat * math.pi/180)
+    y = 0.5 - math.log( (1 + siny)/(1 - siny))/(4 * math.pi)
+
+    mapSize = calcMapSize(zoom)
+
+    pixelX = int()
+
+def getBoundsZoomLevel(bounds, mapDim):
+    WORLD_DIM = { 'height': 256, 'width': 256}
+    ZOOM_MAX = 21
+
+    '''northEast = getNorthEast(bounds)
+    southWest = getSouthWest(bounds)'''
+
+    northEast, southWest = getBoundaries(bounds)
+
+    latFraction = (latRad(northEast['lat']) - latRad(southWest['lat']))/math.pi
+
+    lngDiff = northEast['lng'] - southWest['lng']
+    if lngDiff < 0:
+        lngFraction = (lngDiff + 360)/360
+    else:
+        lngFraction = lngDiff/360
+
+    latZoom = calcZoom(mapDim['height'], WORLD_DIM['height'], latFraction)
+    lngZoom = calcZoom(mapDim['width'], WORLD_DIM['width'], lngFraction)
+
+
+    return min(latZoom, lngZoom, ZOOM_MAX)
+
+def calcBounds(point, zoom, width, height):
+
+    scale = math.pow(2.0, float(zoom))
+
+    centerPx = fromLatLongToPoint({ 'lat': point['lat'], 'lng': point['lng']})
+
+    southWestPoint = { 'x': centerPx['x'] - (width/2)/scale, 'y': centerPx['y'] + (height/2)/scale }
+    southWestLatLng = fromPointToLatLong(southWestPoint)
+
+    northEastPoint = { 'x': centerPx['x'] + (width/2)/scale, 'y': centerPx['y'] - (height/2)/scale }
+    northEastLatLng = fromPointToLatLong(northEastPoint)
+
+    return northEastLatLng, southWestLatLng
+
+def getLatLngtoPixel(latLng, zoom):
+    siny = math.sin(latLng['lat'] * math.pi / 180)
+    pixelX = ((latLng['lng'] + 180) / 360) * 256 * math.pow(2, zoom)
+    pixelY = (0.5 - math.log((1 + siny) / (1 - siny)) / (4 * math.pi)) * 256 * math.pow(2, zoom)
+    return {'x': pixelX, 'y': pixelY }
+
+def getCentre(bounds):
+    centre = { 'lat': 0, 'lng': 0}
+
+    for marker in bounds:
+        centre['lat'] += marker['lat']
+        centre['lng'] += marker['lng']
+
+    centre['lat'] = centre['lat']/len(bounds)
+    centre['lng'] = centre['lng']/len(bounds)
+
+    print('centre is ', json.dumps(centre))
+    return centre
+
+
+
+
+
+
+
